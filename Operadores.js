bf5cbc8ee5fce3ff481ebc283f2aefcdfd4dab56
MartinezSalgadoHectorAdrian <22690037@tecvalles.mx>
2024-11-11 07:29:25 -0600

    Add files via upload

---
+++ Operadores.js
@@ -0,0 +1,15 @@
+ let a = 5;
+ let b = 3;
+
+ let resultado = ((a + b - 2) * 3) / 2;
+ console.log(resultado); // 9
+ resultado += 1;
+
+ console.log(a > b); // true
+ console.log(a === b); // false, se compara el valor y el tipo
+
+ console.log(resultado % 3 == 0 ? "👍" : "👎");
+
+ let cadena = "Hola, " + "mundo!";
+
+ console.log(cadena); // Hola, mundo!
